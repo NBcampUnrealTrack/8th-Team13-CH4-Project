79d4fd9c2ce812b3e2e70f4035cce31ea2d1085b
KYJ <rooooor51@naver.com>
2026-06-29 10:27:48 +0900

    [feat] add GS_GameModeBase

---
+++ Source/Gang_Squirrel/Game/GS_GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GS_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GANG_SQUIRREL_API AGS_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
