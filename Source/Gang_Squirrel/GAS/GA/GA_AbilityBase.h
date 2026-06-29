38b5a2339e608140b48f8fc288d856a7d2d0cae3
gimunkwon <gimon71@gmail.com>
2026-06-29 12:56:00 +0900

    [Add] GA_Base클래스 추가(넷정책,인스턴싱 정책 초기화)

---
+++ Source/Gang_Squirrel/GAS/GA/GA_AbilityBase.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_AbilityBase.generated.h"
+
+UCLASS()
+class GANG_SQUIRREL_API UGA_AbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+	UGA_AbilityBase();
+};
