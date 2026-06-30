0385586cf81f5b769f7c93b251ad65c95079fa47
gimunkwon <gimon71@gmail.com>
2026-06-30 11:18:26 +0900

    [Add] AIController PerceptionComponent 초기화, SenseConfig 설정으로 초기화 BT 트리 연동 로직 추가 SightConfig Update에 따른 BB 키값 변경 델리게이트 바인딩 추가

--- Source/Gang_Squirrel/Enemy/AIController/GS_EnemyAIController.cpp
+++ Source/Gang_Squirrel/Enemy/AIController/GS_EnemyAIController.cpp
@@ -1,21 +1,92 @@
 #include "GS_EnemyAIController.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AIPerceptionTypes.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 
 AGS_EnemyAIController::AGS_EnemyAIController()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	//TODO::DataTable,Asset Refac
+	PerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
+	
+#pragma region SightConfig_Settings
+	Sight_Config = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	Sight_Config->SightRadius = 1500.f;
+	Sight_Config->LoseSightRadius = 2000.f;
+	// *2 = SightDegrees is 360
+	Sight_Config->PeripheralVisionAngleDegrees = 180.f;
+	Sight_Config->SetMaxAge(5.f);
+	// Detection Type
+	Sight_Config->DetectionByAffiliation.bDetectEnemies = true;
+	Sight_Config->DetectionByAffiliation.bDetectFriendlies = true;
+	Sight_Config->DetectionByAffiliation.bDetectNeutrals = true;
+#pragma endregion 
+	
+#pragma region PerceptionCompSettings
+	PerceptionComp->ConfigureSense(*Sight_Config);
+	PerceptionComp->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this,&AGS_EnemyAIController::OnTargetPerceptionUpdated);
+	
+	SetPerceptionComponent(*PerceptionComp);
+#pragma endregion
 }
 
-
-void AGS_EnemyAIController::BeginPlay()
+//For Debugging Sight Radius
+void AGS_EnemyAIController::Tick(float DeltaSeconds)
 {
-	Super::BeginPlay();
+	Super::Tick(DeltaSeconds);
+	
+	DrawDebug_SightRadius();
 }
 
-
-void AGS_EnemyAIController::Tick(float DeltaTime)
+void AGS_EnemyAIController::OnPossess(APawn* InPawn)
 {
-	Super::Tick(DeltaTime);
+	Super::OnPossess(InPawn);
+	
+	if (BT_Enemy)
+	{
+		RunBehaviorTree(BT_Enemy);
+	}
+}
+
+// When Sight Sense Updated(Lose,Detection...)
+void AGS_EnemyAIController::OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	UBlackboardComponent* BB = GetBlackboardComponent();
+	if (!BB)
+	{
+		return;
+	}
+	
+	// Detection Success
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		BB->SetValueAsObject(FName("TargetActor"),Actor);
+	}
+	// Detection Failed
+	else
+	{
+		BB->ClearValue(FName("TargetActor"));
+	}
+}
+
+void AGS_EnemyAIController::DrawDebug_SightRadius()
+{
+	//TODO::Migrate to EnemyClass Tick
+	// if ListenServer Settings Can See DebugLine
+	APawn* OwnerPawn = GetPawn();
+	if (!OwnerPawn || !Sight_Config)
+	{
+		return;
+	}
+	
+	// Accept Sense SightRadius
+	DrawDebugSphere(GetWorld(),OwnerPawn->GetActorLocation(),Sight_Config->SightRadius,32,FColor::Green,false);
+	// Lose Sense SightRadius
+	DrawDebugSphere(GetWorld(),OwnerPawn->GetActorLocation(),Sight_Config->LoseSightRadius,32,FColor::Red,false);
 }
 
--- Source/Gang_Squirrel/Enemy/AIController/GS_EnemyAIController.h
+++ Source/Gang_Squirrel/Enemy/AIController/GS_EnemyAIController.h
@@ -4,14 +4,30 @@
 #include "AIController.h"
 #include "GS_EnemyAIController.generated.h"
 
+class UAISenseConfig_Sight;
+struct FAIStimulus;
+//TODO::AIPerceptionComp,SightConfig,BTRun
 UCLASS()
 class GANG_SQUIRREL_API AGS_EnemyAIController : public AAIController
 {
 	GENERATED_BODY()
 public:
 	AGS_EnemyAIController();
+	
 protected:
-	virtual void BeginPlay() override;
-public:
-	virtual void Tick(float DeltaTime) override;
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void Tick(float DeltaSeconds) override;
+	
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	
+	UPROPERTY(EditDefaultsOnly,Category="AI|BT")
+	TObjectPtr<UBehaviorTree> BT_Enemy;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAIPerceptionComponent> PerceptionComp;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAISenseConfig_Sight> Sight_Config;
+private:
+	//Debugging Sight Radius Func
+	void DrawDebug_SightRadius();
 };
