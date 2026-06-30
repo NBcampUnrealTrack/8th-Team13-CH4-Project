56cc9bf89848df1fa9f7d077a901f82e1d099472
gimunkwon <gimon71@gmail.com>
2026-06-30 12:10:54 +0900

    [Add,Feat] BB 키값 TargetActor를 통해 MoveToActor 테스크 노드를 실행할 수 있도록 로직 작성 MoveToActor RequestID 값이 성공적으로 도달했을때 BB키값 bCanAttack을 수정하여 공격가능한 상태로 전이 하게 하는 로직 추가

--- Source/Gang_Squirrel/Enemy/TaskNode/BTTask_Chase.cpp
+++ Source/Gang_Squirrel/Enemy/TaskNode/BTTask_Chase.cpp
@@ -1,1 +1,58 @@
 #include "BTTask_Chase.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_Chase::UBTTask_Chase()
+{
+	NodeName = TEXT("Chase_Target");
+	TargetActorKey.AddObjectFilter(this,GET_MEMBER_NAME_CHECKED(UBTTask_Chase,TargetActorKey),AActor::StaticClass());
+}
+
+EBTNodeResult::Type UBTTask_Chase::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* OwnerAIController = OwnerComp.GetAIOwner();
+	UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
+	
+	if (!OwnerAIController || !BB)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	AActor* Target = Cast<AActor>(BB->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (!Target)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	FAIRequestID RequestID = OwnerAIController->MoveToActor(Target,AcceptanceRadius);
+	
+	if (RequestID.IsValid())
+	{
+		WaitForMessage(OwnerComp, UBrainComponent::AIMessage_MoveFinished,RequestID);
+		
+		return EBTNodeResult::InProgress;
+	}
+	
+	return EBTNodeResult::Failed;
+}
+
+void UBTTask_Chase::OnMessage(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, FName Message, int32 RequestID,
+	bool bSuccess)
+{
+	Super::OnMessage(OwnerComp, NodeMemory, Message, RequestID, bSuccess);
+	
+	UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
+	
+	if (bSuccess && BB)
+	{
+		// When Enemy Reached to TargetActor
+		BB->SetValueAsBool(FName("bCanAttack"),true);
+		FinishLatentTask(OwnerComp,EBTNodeResult::Succeeded);
+	}
+	else
+	{
+		FinishLatentTask(OwnerComp,EBTNodeResult::Failed);
+	}
+	
+}
--- Source/Gang_Squirrel/Enemy/TaskNode/BTTask_Chase.h
+++ Source/Gang_Squirrel/Enemy/TaskNode/BTTask_Chase.h
@@ -8,4 +8,15 @@ UCLASS()
 class GANG_SQUIRREL_API UBTTask_Chase : public UBTTaskNode
 {
 	GENERATED_BODY()
+public:
+	UBTTask_Chase();
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void OnMessage(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, FName Message, int32 RequestID, bool bSuccess) override;
+	
+	UPROPERTY(EditAnywhere,Category="BlackBoard|ObjectKey")
+	FBlackboardKeySelector TargetActorKey;
+	// TODO::Refac to DataTable
+	UPROPERTY(EditAnywhere,Category="AI")
+	float AcceptanceRadius = 150.f;
 };
