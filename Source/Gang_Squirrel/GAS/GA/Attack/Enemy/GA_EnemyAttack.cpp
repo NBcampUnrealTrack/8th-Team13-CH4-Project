4c1d225181cf08eef9fc22cb3501180fb2b55ef9
gimunkwon <gimon71@gmail.com>
2026-07-01 12:07:33 +0900

    [Feat] GA_EnemyAttack 구현 방식은 GA_Attack과 동일하나 클라이언트와 서버와의 핸드셰이크 과정이 빠짐(Enemy 같은 경우에는 서버에서만 로직이 작동하므로)

--- Source/Gang_Squirrel/GAS/GA/Attack/Enemy/GA_EnemyAttack.cpp
+++ Source/Gang_Squirrel/GAS/GA/Attack/Enemy/GA_EnemyAttack.cpp
@@ -1,5 +1,11 @@
 #include "GA_EnemyAttack.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Components/SphereComponent.h"
+#include "Gang_Squirrel/Enemy/GS_Enemy.h"
+
 UGA_EnemyAttack::UGA_EnemyAttack()
 {
 }
@@ -9,23 +15,115 @@ void UGA_EnemyAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	AGS_Enemy* Enemy = Cast<AGS_Enemy>(GetAvatarActorFromActorInfo());
+	if (!Enemy)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	HitActors.Empty();
+	
+	if (ActorInfo->IsNetAuthority())
+	{
+		EnableAttackCollision(Enemy,true);
+	}
+	
+	if (AM_Attack)
+	{
+		UAbilityTask_PlayMontageAndWait* TaskMontage = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this,NAME_None,AM_Attack);
+		
+		TaskMontage->OnCompleted.AddDynamic(this,&UGA_EnemyAttack::K2_EndAbility);
+		TaskMontage->OnCancelled.AddDynamic(this,&UGA_EnemyAttack::K2_EndAbility);
+		TaskMontage->OnInterrupted.AddDynamic(this,&UGA_EnemyAttack::K2_EndAbility);
+		
+		TaskMontage->ReadyForActivation();
+	}
 }
 
 void UGA_EnemyAttack::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
+	AGS_Enemy* Enemy = Cast<AGS_Enemy>(GetAvatarActorFromActorInfo());
+	
+	if (Enemy && ActorInfo->IsNetAuthority())
+	{
+		EnableAttackCollision(Enemy,false);
+	}
+	
+	HitActors.Empty();
+	
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
 void UGA_EnemyAttack::OnAttackOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!OtherActor || OtherActor == GetAvatarActorFromActorInfo())
+	{
+		return;
+	}
+	
+	if (HitActors.Contains(OtherActor))
+	{
+		return;
+	}
+	
+	HitActors.Add(OtherActor);
+	
+	ApplyDamageToTarget(OtherActor);
 }
 
 void UGA_EnemyAttack::EnableAttackCollision(AGS_Enemy* OwnerEnemy, bool bEnable)
 {
+	auto OnCollision = [&](USphereComponent* HandCollision)
+	{
+		if (!HandCollision)
+		{
+			return;
+		}
+		
+		HandCollision->SetCollisionEnabled(bEnable ? ECollisionEnabled::QueryOnly : ECollisionEnabled::NoCollision);
+		
+		if (bEnable)
+		{
+			HandCollision->OnComponentBeginOverlap.AddDynamic(this, &UGA_EnemyAttack::OnAttackOverlap);
+		}
+		else
+		{
+			HandCollision->OnComponentBeginOverlap.RemoveDynamic(this, &UGA_EnemyAttack::OnAttackOverlap);
+		}
+	};
+	
+	OnCollision(OwnerEnemy->GetCombatCollision(EHandCombatType::LeftCombatHand));
+	OnCollision(OwnerEnemy->GetCombatCollision(EHandCombatType::RightCombatHand));
 }
 
 void UGA_EnemyAttack::ApplyDamageToTarget(AActor* TargetActor)
 {
+	if (!GE_Damage || !TargetActor)
+	{
+		return;
+	}
+	
+	UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo();
+	
+	FGameplayEffectContextHandle GEContextHandle = SourceASC->MakeEffectContext();
+	GEContextHandle.AddSourceObject(this);
+	
+	FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(GE_Damage,1.f,GEContextHandle);
+	
+	if (!SpecHandle.IsValid())
+	{
+		return;
+	}
+	
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (TargetActor && TargetASC)
+	{
+		SourceASC->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(),TargetASC);
+	}
+	
+	
 }
