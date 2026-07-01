b787379653b9156714e2b54c7fe4fd56050437b4
gimunkwon <gimon71@gmail.com>
2026-07-01 11:33:15 +0900

    [Add] GA_EnemyAttack 클래스 추가

---
+++ Source/Gang_Squirrel/GAS/GA/Attack/Enemy/GA_EnemyAttack.cpp
@@ -0,0 +1,31 @@
+#include "GA_EnemyAttack.h"
+
+UGA_EnemyAttack::UGA_EnemyAttack()
+{
+}
+
+void UGA_EnemyAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_EnemyAttack::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_EnemyAttack::OnAttackOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void UGA_EnemyAttack::EnableAttackCollision(AGS_Enemy* OwnerEnemy, bool bEnable)
+{
+}
+
+void UGA_EnemyAttack::ApplyDamageToTarget(AActor* TargetActor)
+{
+}
