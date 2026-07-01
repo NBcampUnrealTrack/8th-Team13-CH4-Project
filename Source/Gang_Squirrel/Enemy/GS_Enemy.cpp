7cb9d9de8b4832dfee21a85b90f03565d49cc570
gimunkwon <gimon71@gmail.com>
2026-07-01 11:08:40 +0900

    [Add] Enemy 클래스 AttackCollision추가 (Player 와 동일하게 양손으로 부착)

--- Source/Gang_Squirrel/Enemy/GS_Enemy.cpp
+++ Source/Gang_Squirrel/Enemy/GS_Enemy.cpp
@@ -1,6 +1,7 @@
 #include "GS_Enemy.h"
 
 #include "AbilitySystemComponent.h"
+#include "Components/SphereComponent.h"
 #include "Gang_Squirrel/GAS/AttributeSet/GS_PlayerAttributeSet.h"
 
 
@@ -8,6 +9,7 @@ AGS_Enemy::AGS_Enemy()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	
+#pragma region InitGASSystem
 	//Create ASC
 	EnemyAbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
 	EnemyAbilitySystemComp->SetIsReplicated(true);
@@ -15,8 +17,17 @@ AGS_Enemy::AGS_Enemy()
 	
 	//Create AttributeSet
 	EnemyAttributeSet = CreateDefaultSubobject<UGS_PlayerAttributeSet>(TEXT("AttributeSet"));
+#pragma endregion 
+	
+#pragma region CombatComponent
+	//Create CombatComponent
+	SphereComp_LeftHand = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent_LeftHand"));
+	SphereComp_LeftHand->SetupAttachment(GetMesh(), FName("L_Hand"));
+	
+	SphereComp_RightHand = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent_RightHand"));
+	SphereComp_RightHand->SetupAttachment(GetMesh(), FName("R_Hand"));
+#pragma endregion
 }
-
 
 void AGS_Enemy::BeginPlay()
 {
@@ -28,7 +39,6 @@ void AGS_Enemy::BeginPlay()
 	}
 }
 
-
 void AGS_Enemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -39,3 +49,18 @@ UAbilitySystemComponent* AGS_Enemy::GetAbilitySystemComponent() const
 	return EnemyAbilitySystemComp;
 }
 
+USphereComponent* AGS_Enemy::GetCombatCollision(EHandCombatType HandType) const
+{
+	USphereComponent* CombatHandComp = nullptr;
+	
+	if (HandType == EHandCombatType::RightCombatHand)
+	{
+		CombatHandComp = SphereComp_RightHand;
+	}
+	else
+	{
+		CombatHandComp = SphereComp_LeftHand;
+	}
+	
+	return CombatHandComp;
+}
--- Source/Gang_Squirrel/Enemy/GS_Enemy.h
+++ Source/Gang_Squirrel/Enemy/GS_Enemy.h
@@ -5,8 +5,18 @@
 #include "GameFramework/Character.h"
 #include "GS_Enemy.generated.h"
 
+class USphereComponent;
 class UGameplayAbility;
 class UGS_PlayerAttributeSet;
+
+UENUM()
+enum class EHandCombatType : uint8
+{
+	RightCombatHand,
+	LeftCombatHand
+};
+
+
 
 //TODO::Attach AIController, BB,BT
 UCLASS()
@@ -22,13 +32,24 @@ public:
 	
 	FORCEINLINE TSubclassOf<UGameplayAbility> GetGA_Attack() const { return GA_Attack; }
 	
+	USphereComponent* GetCombatCollision(EHandCombatType HandType) const;
+	
 protected:
 	virtual void BeginPlay() override;
 	
+#pragma region GAS
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
 	TObjectPtr<UAbilitySystemComponent> EnemyAbilitySystemComp;
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
 	TObjectPtr<UGS_PlayerAttributeSet> EnemyAttributeSet;
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="GAS|GameplayAbility")
 	TSubclassOf<UGameplayAbility> GA_Attack;
+#pragma endregion
+	
+#pragma region CombatComp
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereComp_LeftHand;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereComp_RightHand;
+#pragma endregion 
 };
