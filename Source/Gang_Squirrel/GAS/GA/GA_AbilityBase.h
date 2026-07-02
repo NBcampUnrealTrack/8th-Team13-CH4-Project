dead4abe73f62a9d916baea866153cc1b0446eeb
gimunkwon <gimon71@gmail.com>
2026-07-02 17:48:37 +0900

    [Add] 같은 팀인지(태그가 동일한지) 확인하는 정적메서드 추가(팀킬방지용)

--- Source/Gang_Squirrel/GAS/GA/GA_AbilityBase.h
+++ Source/Gang_Squirrel/GAS/GA/GA_AbilityBase.h
@@ -10,4 +10,7 @@ class GANG_SQUIRREL_API UGA_AbilityBase : public UGameplayAbility
 	GENERATED_BODY()
 public:
 	UGA_AbilityBase();
+	
+protected:
+	static bool IsSameTeam(AActor* SourceActor, AActor* TargetActor);
 };
