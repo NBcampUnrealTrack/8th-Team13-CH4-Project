fa156fae397a900fd6645f772eb9056fbab7da16
gimunkwon <gimon71@gmail.com>
2026-07-02 19:53:36 +0900

    [Fix] HandleRespawn 메서드가 호출 되지 않던 문제 Fix

--- Source/Gang_Squirrel/GAS/GA/Death/GA_PlayerDeath.cpp
+++ Source/Gang_Squirrel/GAS/GA/Death/GA_PlayerDeath.cpp
@@ -7,6 +7,7 @@
 #include "GameFramework/PlayerStart.h"
 #include "Gang_Squirrel/Character/GSCharacter.h"
 #include "Gang_Squirrel/GAS/Tags/GS_GamePlayTag.h"
+#include "Gang_Squirrel/Gang_Squirrel.h"
 
 UGA_PlayerDeath::UGA_PlayerDeath()
 {
@@ -19,32 +20,39 @@ void UGA_PlayerDeath::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	
+
+	UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] ActivateAbility called - Player:%s, AM_Death:%s"), *GetNameSafe(GetAvatarActorFromActorInfo()), AM_Death ? TEXT("valid") : TEXT("NULL"));
+
 	AGSCharacter* PlayerCharacter = Cast<AGSCharacter>(GetAvatarActorFromActorInfo());
 	if (!PlayerCharacter)
 	{
+		UE_LOG(LogGAS, Error, TEXT("[GA_PlayerDeath] Cast to AGSCharacter FAILED"));
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
 	}
-	
+
+	UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] IsNetAuthority:%s"), ActorInfo->IsNetAuthority() ? TEXT("true") : TEXT("false"));
+
 	if (ActorInfo->IsNetAuthority())
 	{
 		PlayerCharacter->GetCharacterMovement()->DisableMovement();
-		
+
 		if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
 		{
-			// AddReplicatedLooseGameplayTag만으로는 서버 자신의 GameplayTagCountContainer에 반영 안 됨(클라 복제용 별도 맵) - AddLooseGameplayTag로 즉시 반영 필요
 			ASC->AddLooseGameplayTag(StateTag::TAG_State_Dead);
 			ASC->AddReplicatedLooseGameplayTag(StateTag::TAG_State_Dead);
 		}
-		
+
 		// TempLogic
+		bool bFoundPlayerStart = false;
 		for (TActorIterator<APlayerStart> It(GetWorld()); It; ++It)
 		{
 			CachedRespawnLocation = It->GetActorLocation();
 			CachedRespawnRotation = It->GetActorRotation();
+			bFoundPlayerStart = true;
 			break;
 		}
+		UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] PlayerStart found:%s, CachedRespawnLocation:%s"), bFoundPlayerStart ? TEXT("true") : TEXT("false"), *CachedRespawnLocation.ToString());
 	}
 	
 	if (AM_Death)
@@ -65,45 +73,61 @@ void UGA_PlayerDeath::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 void UGA_PlayerDeath::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
+	UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] EndAbility called - Player:%s, IsNetAuthority:%s, bWasCancelled:%s"),
+		*GetNameSafe(GetAvatarActorFromActorInfo()), ActorInfo->IsNetAuthority() ? TEXT("true") : TEXT("false"), bWasCancelled ? TEXT("true") : TEXT("false"));
+
 	if (AGSCharacter* PlayerCharacter = Cast<AGSCharacter>(GetAvatarActorFromActorInfo()))
 	{
 		if (ActorInfo->IsNetAuthority())
 		{
-			GetWorld()->GetTimerManager().SetTimer(RespawnTimerHandle,this,&UGA_PlayerDeath::HandleRespawn,RespawnDelay,false);
+			TWeakObjectPtr<UGA_PlayerDeath> WeakThis(this);
+			GetWorld()->GetTimerManager().SetTimer(RespawnTimerHandle,[WeakThis]()
+			{
+				if (WeakThis.IsValid())
+				{
+					WeakThis->HandleRespawn();
+				}
+			},RespawnDelay,false);
 		}
 	}
-	
+
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
 // Temp Method
 void UGA_PlayerDeath::HandleRespawn()
 {
+	UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] HandleRespawn called - Player:%s, CachedRespawnLocation:%s"), *GetNameSafe(GetAvatarActorFromActorInfo()), *CachedRespawnLocation.ToString());
+
 	ACharacter* PlayerChar = Cast<ACharacter>(GetAvatarActorFromActorInfo());
 	if (!PlayerChar)
 	{
+		UE_LOG(LogGAS, Error, TEXT("[GA_PlayerDeath] HandleRespawn - Cast to ACharacter FAILED"));
 		return;
 	}
-	
+
 	PlayerChar->SetActorLocation(CachedRespawnLocation);
 	PlayerChar->SetActorRotation(CachedRespawnRotation);
 	PlayerChar->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
-	
+
 	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo())
 	{
 		ASC->RemoveLooseGameplayTag(StateTag::TAG_State_Dead);
 		ASC->RemoveReplicatedLooseGameplayTag(StateTag::TAG_State_Dead);
-		
+
+		UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] HandleRespawn - GE_Respawn:%s"), GE_Respawn ? *GE_Respawn->GetName() : TEXT("NULL"));
+
 		if (GE_Respawn)
 		{
 			FGameplayEffectContextHandle ContextHandle = ASC->MakeEffectContext();
 			FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(GE_Respawn,1,ContextHandle);
-			
+
 			if (SpecHandle.IsValid())
 			{
-				ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+				FActiveGameplayEffectHandle AppliedHandle = ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+				UE_LOG(LogGAS, Warning, TEXT("[GA_PlayerDeath] HandleRespawn - GE_Respawn applied, WasSuccessfullyApplied:%s"), AppliedHandle.WasSuccessfullyApplied() ? TEXT("true") : TEXT("false"));
 			}
 		}
 	}
-	
+
 }
