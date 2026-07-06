40ddea451143d3b76838556db985febf9ca1004b
gimunkwon <gimon71@gmail.com>
2026-07-06 20:15:51 +0900

    [Add] EnemyDataTable 추가

---
+++ Source/Gang_Squirrel/DataBase/DataTable/DT_Enemy.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "DT_Enemy.generated.h"
+
+
+class UGameplayAbility;
+
+USTRUCT(BlueprintType)
+struct FGS_EnemyDataTable : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	// Perception
+	UPROPERTY(EditAnywhere,Category="Perception")
+	float SightRadius = 50.f;
+	UPROPERTY(EditAnywhere,Category="Perception")
+	float LoseSightRadius = 100.f;
+	
+	// Chase
+	UPROPERTY(EditAnywhere,Category="Chase")
+	float AcceptanceRadius = 30.f;
+	UPROPERTY(EditAnywhere,Category="Chase")
+	float ChaseRotationInterpSpeed = 30.f;
+	
+	// Attack
+	UPROPERTY(EditAnywhere,Category="Attack")
+	float FacingToleranceDegrees = 10.f;
+	UPROPERTY(EditAnywhere,Category="Attack")
+	float AttackRotationInterpSpeed = 30.f;
+	
+	// Stat
+	UPROPERTY(EditAnywhere,Category="Stat")
+	float Health = 3.f;
+	UPROPERTY(EditAnywhere,Category="Stat")
+	float MaxHealth = 3.f;
+	UPROPERTY(EditAnywhere,Category="Stat")
+	float MoveSpeed = 600.f;
+	
+	// Ability
+	UPROPERTY(EditAnywhere,Category="Ability")
+	TArray<TSubclassOf<UGameplayAbility>> GrantedAbilities;
+};
+
+
