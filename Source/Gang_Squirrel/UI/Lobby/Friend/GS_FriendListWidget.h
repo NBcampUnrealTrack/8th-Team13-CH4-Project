05c7f184763c77db0ce0cebcdfbd6ca2f0cccf87
gimunkwon <gimon71@gmail.com>
2026-07-11 13:11:22 +0900

    [Add] 친구창 위젯 추가

---
+++ Source/Gang_Squirrel/UI/Lobby/Friend/GS_FriendListWidget.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GS_FriendListWidget.generated.h"
+
+
+class UGS_FriendEntryWidget;
+
+UCLASS()
+class GANG_SQUIRREL_API UGS_FriendListWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UPanelWidget> EntryContainer;
+	
+	UPROPERTY(EditDefaultsOnly,Category="UI")
+	TSubclassOf<UGS_FriendEntryWidget> EntryWidgetClass;
+	
+private:
+	UFUNCTION()
+	void OnFriendsListComplete(bool bWasSuccessful);
+	
+	void RefreshEntries();
+};
